fb8c08182f7734873bd9b90e6c6b9e28e1a002ee
Hannes Sachsenmaier <1910653347@stud.fh-kufstein.ac.at>
2020-03-25 17:20:12 +0100

    Uebung6 Aufgabe 1 erledigt

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
 <?php
 
-//@TODO insert your code here
+require_once ('includes/classes/Database.php');
+
+define('DB_HOST','localhost');
+define('DB_NAME','ue06');
+define('DB_USER','testuser');
+define('DB_PASS','testpass');
+
+$db = new Database();
+
+$cryptedPassword = password_hash('testpassword',PASSWORD_BCRYPT);
+$username = 'test';
+
+$cryptedPassword = $db->escapeString($cryptedPassword);
+$username = $db->escapeString($username);
+
+//$sql = "Insert into user(name,`password`)values('.$username.','.$cryptedPassword')";
+
+//$db ->query($sql);
+
+$sql = "Select * from user where name='.$username.'";
+$result = $db->query($sql);
+
+if($db->numRows($result)>0){
+    $row = $db->fetchAssoc($result);
+
+    if (password_verify("testpassword",$row['password'])){
+        echo "Der Nutzer".$username." mit der ID ".$row['id']." hat";
+        echo " das Passwort testpassword";
+    }else{
+        echo "Nutzer gefunden aber falsches Passwort";
+    }
+}else{
+    echo "Keinen Nutzer gefunden";
+}
+
+
+
+
+
